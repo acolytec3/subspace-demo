3c0ad32e6a849fc6286f01b49c1b93d6dd4538cb
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-03-12 14:42:50 -0400

    Add subspace subscriptions in useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import {Box, Button, Grommet, List, Text} from 'grommet';
 import Subspace from '@embarklabs/subspace';
 import Web3 from 'web3';
@@ -9,6 +9,7 @@ const web3 = new Web3('https://mainnet.infura.io/v3/9eb527726b034638b37f37f66b0f
 const subspace = new Subspace(web3.currentProvider);
 var dai = new web3.eth.Contract(exchangeABI,'0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667');
 const daiContract = subspace.contract(dai);
+subspace.init();
 
 const theme = {
     global: {
@@ -23,30 +24,43 @@ const theme = {
 
 function App() {
 
-    const [trades, updateTrades] = useState();
-
+    const [trades, updateTrades] = useState({exchangeRate:1.0});
+    const [txnObserver, setObservable] = useState();
+    const [latestBlock, setBlock] = useState();
     function TradeDetails (tokensSold, ethBought) {
         this.tokensSold = web3.utils.fromWei(tokensSold);
         this.ethBought = web3.utils.fromWei(ethBought);
         this.exchangeRate = this.tokensSold/this.ethBought;
     }
 
-    async function startListener () {
-        await subspace.init();
-        const latest = await web3.eth.getBlockNumber();
-        var EthPurchased$ = daiContract.events.EthPurchase.track({fromBlock:latest - 50});
-        EthPurchased$.subscribe(function (trade) {
-            const txnDetails = new TradeDetails(trade.tokens_sold, trade.eth_bought);
-            console.log(txnDetails)
-            updateTrades(txnDetails);
-            console.log(trades);
-        })
-      }
+    useEffect (() => {
+        web3.eth.getBlockNumber().then((block) => setBlock(block));
+        console.log(typeof latestBlock)
+        if (typeof(latestBlock) != "number")
+            return;
+        const EthPurchased$ = daiContract.events.EthPurchase.track({fromBlock: latestBlock - 10});
+        setObservable(EthPurchased$);
+      },[setObservable, latestBlock])
 
+    useEffect (() => {
+        if((txnObserver === undefined) || (typeof latestBlock != "number")) {
+            return;
+        }
+        else {
+            txnObserver.subscribe((trade) => {
+                console.log(trade);
+                const txnDetails = new TradeDetails(trade.tokens_sold, trade.eth_bought);
+                updateTrades(txnDetails);
+        }
+        )};
+        return txnObserver.unsubscribe;
+    },[txnObserver, latestBlock]);
+
+    
     return (
         <Grommet theme={theme}>
             <AppBar>Hello Subspace!</AppBar>
-            <Button label="Start Listening" onClick={() => startListener() }></Button>
+            <Text>Exchange rate on most recent ETH/DAI trade = {trades.exchangeRate}</Text>
         </Grommet>
     );
 }
